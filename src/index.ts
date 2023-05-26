feefeaff7e08aeedfb8c367a4a0e39562b223421
fs-public <134224417+fs-public@users.noreply.github.com>
2023-05-26 19:58:33 +0200

    Separate manager logic

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-import { HIT_BRACKETS, MAX_WORKER_THREADS, MESSAGES_DIRECTORY } from "./config"
-import { getAllFiles, getDistFile, readFile } from "./utils"
+import { HIT_BRACKETS, MESSAGES_DIRECTORY } from "./config"
+import { getAllFiles, readFile } from "./utils"
 import { getBracket, getBracketName } from "./brackets"
-import { Worker } from "worker_threads"
-import { UserHits } from "./types"
-
-let analyzedFiles = 0
-
-const spawnWorker = (workerData: string[]): Promise<UserHits> => {
-    return new Promise((resolve, reject) => {
-        const worker = new Worker(getDistFile("./workers/analysis"), { workerData })
-        worker.on("message", (data) => {
-            if (typeof data === "number") analyzedFiles += data
-            else resolve(data)
-        })
-        worker.on("error", (message) => reject(message))
-    })
-}
+import { analyzeConcurrently } from "./workers/manager"
 
 const main = async () => {
     // Initialize /////////////////////////////////////////
@@ -29,28 +15,7 @@ const main = async () => {
 
     // Analyze /////////////////////////////////////////
 
-    const totalThreads = Math.min(Math.round(files.length / 25), MAX_WORKER_THREADS)
-    const filesPerThread = Math.ceil(files.length / totalThreads)
-
-    console.log("Spawning", totalThreads, "worker threads")
-
-    const workers = [...Array(totalThreads)]
-        .fill(undefined)
-        .map((_, i) => spawnWorker(contents.slice(i * filesPerThread, (i + 1) * filesPerThread)))
-
-    const progressInterval = setInterval(() => console.log("Analyzed", analyzedFiles, "files..."), 1000)
-
-    const results = await Promise.all(workers)
-
-    clearInterval(progressInterval)
-
-    const userHits: UserHits = {}
-
-    results.forEach((resultSlice) => {
-        Object.keys(resultSlice).forEach((key) => {
-            userHits[key] = (userHits[key] || 0) + (resultSlice[key] || 0)
-        })
-    })
+    const userHits = await analyzeConcurrently(contents)
 
     const users = Object.keys(userHits)
 
